74704304aa969cd98ea96a6e5710fd7069f370d4
David H <hoong.way2018@gmail.com>
2019-11-17 22:16:05 -0800

    finished

---
+++ ConsoleApp1/ConsoleApp1/GetshortenUrl.cs
@@ -0,0 +1,86 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    public static class GetshortenUrl
+    {
+        public static void Get(ChromeDriver driver)
+        {
+            //var driver = new ChromeDriver();
+            for (int i = 0; i < 11; i++)
+            {
+                var tempstr = Guid.NewGuid().ToString().Replace("-", "");
+                tempstr = tempstr.Substring(tempstr.Length / 2);
+                string longurl = "https://github.com/begood0513/goodnews/blob/master/README.md?" + tempstr;
+
+                //is.gd
+                driver.Url = "https://is.gd/";
+                var input = driver.FindElement(By.Name("url"));
+                input.Clear();
+                input.SendKeys(longurl);
+                driver.FindElement(By.ClassName("submitbutton")).Click();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);     //等待10s
+                //var shortenurl = driver.FindElement(By.Id("short_url")).GetCssValue("value");
+                var shortenurl = driver.FindElement(By.Id("short_url")).GetAttribute("value");      //GetAttribute get values of attribute : https://chercher.tech/java/get-text-css-attribute-size-value-selenium-webdriver
+                Console.WriteLine(shortenurl);
+
+
+                //tinyurl.com 有广告
+                //driver.Navigate().GoToUrl("https://tinyurl.com/");
+                //input = driver.FindElement(By.Id("url"));
+                //input.Clear();
+                //input.SendKeys(longurl);
+                //string cssselector= "#f>input[type="+"submit"+"]";      //CssSelector: https://www.w3school.com.cn/cssref/css_selectors.asp
+                //driver.FindElement(By.CssSelector(cssselector)).Click();
+                //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);     //等待10s
+                //shortenurl = "";
+                //shortenurl =driver.FindElement(By.Id("copy_div")).GetAttribute("href");
+                //Console.WriteLine(shortenurl);
+
+                //https://bitly.com/
+                //driver.Navigate().GoToUrl("https://bitly.com/");
+                //input = driver.FindElement(By.Id("shorten_url"));
+                //input.Clear();
+                //input.SendKeys(longurl);
+                //driver.FindElement(By.Id("shorten_btn")).Click();
+                //Thread.Sleep(10000);
+                ////driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);     //等待10s
+                //shortenurl = "";
+                //var result = driver.FindElement(By.Id("shortened_url"));
+                //Console.WriteLine(shortenurl);
+
+                //https://www.shorturl.at/
+                driver.Navigate().GoToUrl("https://www.shorturl.at/");
+                input = driver.FindElement(By.Name("u"));
+                input.Clear();
+                input.SendKeys(longurl);
+                string cssselector = "#formbutton>input[type=" + "submit" + "]";
+                driver.FindElement(By.CssSelector(cssselector)).Click();
+                //Thread.Sleep(10000);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);     //等待10s
+                shortenurl = "";
+                shortenurl = driver.FindElement(By.Id("shortenurl")).GetAttribute("value");
+                Console.WriteLine(shortenurl);
+
+                //https://www.rebrandly.com/    有防护
+                //driver.Navigate().GoToUrl("https://www.rebrandly.com/");
+                //input = driver.FindElement(By.Name("url"));
+                //input.Clear();
+                //input.SendKeys(longurl);
+                //cssselector = "#wf-form-rebrand-link-form>input[value=" + "Shorten" + "]";
+                //driver.FindElement(By.CssSelector(cssselector)).Click();
+                ////Thread.Sleep(10000);
+                //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);     //等待10s
+                //shortenurl = "";
+                //shortenurl = driver.FindElement(By.Id("shorten-2")).GetAttribute("value");
+                //Console.WriteLine(shortenurl);
+
+
+            }
+            driver.Close();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            /*
+             * 详见:
+             * https://www.cnblogs.com/zhaotianff/p/11330810.html
+             * https://juejin.im/entry/5b863bd2f265da437a46a240
+             * https://www.jianshu.com/p/97cc1e5434e9   等待时间
+             * GetAttribute get values of attribute : https://chercher.tech/java/get-text-css-attribute-size-value-selenium-webdriver
+             * CssSelector 语法: https://www.w3school.com.cn/cssref/css_selectors.asp
+            */
+
+            using (var driver = new ChromeDriver())
+            {
+
+                GetshortenUrl.Get(driver);
+                //driver.Navigate().GoToUrl("http://www.baidu.com");  //driver.Url = "http://www.baidu.com"是一样的
+                driver.Url = "https://www.baidu.com";
+                var input = driver.FindElement(By.Id("kw"));
+                // 在输入前清空内容
+                input.Clear();
+                input.SendKeys("Selenium");
+                var submit = driver.FindElement(By.Id("su"));
+                submit.Click();
+                var source = driver.PageSource;
+                //关闭浏览器
+                driver.Close();
+                Console.WriteLine(source);
+                Console.ReadLine();
+            }
+        }
+    }
+}
